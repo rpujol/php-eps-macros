4f56c86e085be541317fd041e8fe681347b4d1de
Romaric Pujol <romaric.pujol@insa-lyon.fr>
2013-09-28 20:31:32 +0200

    Better arrow-head.php

---
+++ arrow-head.php
@@ -0,0 +1,15 @@
+/ArrowHeadAngle 15 def
+/ArrowHeadProportion 1.67 def
+/ArrowHeadHorizontalSize 30 def
+/ArrowHead {
+   % angle X Y ArrowHead
+   gsave
+      translate
+      180 add rotate
+      0 0 moveto
+      ArrowHeadHorizontalSize ArrowHeadProportion mul dup ArrowHeadAngle cos mul exch ArrowHeadAngle sin mul 2 copy lineto
+      ArrowHeadHorizontalSize 0 lineto
+      neg lineto closepath
+      fill
+   grestore
+} bind def
