4e3e0a11065b8a1cac1a80086706013d9669a457
Romaric Pujol <romaric.pujol@insa-lyon.fr>
2013-12-08 19:04:41 +0100

    Added trigonometry.php

---
+++ php/trigonometry.php
@@ -0,0 +1,7 @@
+<?php
+   /* Just basic trigonometry in radians */
+?>
+/pi <?php echo M_PI; ?> def
+/sinr { 180 mul pi div sin } bind def
+/cosr { 180 mul pi div cos } bind def
+/tanr { 180 mul pi div tan } bind def
