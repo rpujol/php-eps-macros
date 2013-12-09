830fd2b4f48b4a8270e4a92038d2f90ed4279ef6
Romaric Pujol <romaric.pujol@insa-lyon.fr>
2013-12-09 14:39:01 +0100

    Added asinr and acosr in trigonometry.php

--- php/trigonometry.php
+++ php/trigonometry.php
@@ -6,3 +6,17 @@
 /cosr { 180 mul pi div cos } bind def
 /tanr { 180 mul pi div tan } bind def
 /atanr { atan pi mul 180 div } bind def
+/acosr {
+   % Using formula: acos(x)=atan2(sqrt(1-x^2),x)
+   dup dup % x x x
+   mul % x x^2
+   neg 1 add sqrt % x 1-x^2
+   exch atanr
+} bind def
+/asinr {
+   % Using formula: asin(x)=atan2(x,sqrt(1-x^2))
+   dup dup % x x x
+   mul % x x^2
+   neg 1 add sqrt % x 1-x^2
+   atanr
+} bind def
