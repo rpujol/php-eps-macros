79ae769212cc5963db22cad546308a8f77ccd77f
Romaric Pujol <romaric.pujol@insa-lyon.fr>
2014-09-21 18:52:35 +0200

    added tan function in trigonometry

--- php/trigonometry.php
+++ php/trigonometry.php
@@ -2,6 +2,7 @@
    /* Just basic trigonometry in radians */
 ?>
 /pi <?php echo M_PI; ?> def
+/tan { dup sin exch cos div } bind def
 /sinr { 180 mul pi div sin } bind def
 /cosr { 180 mul pi div cos } bind def
 /tanr { 180 mul pi div tan } bind def
